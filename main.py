c081bec6d72ec653e9c7d0c85f332f57e0600c3e
Santi Acevedo <117226776+Und3rW0rld@users.noreply.github.com>
2023-02-17 14:00:28 -0500

    Solución taller 2 python

---
+++ main.py
@@ -0,0 +1,41 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    def cambiarColor(self, color):
+        color = color.lower()
+        if color in ["rojo", "amarillo", "verde", "negro", "blanco"]:
+            self.color = color
+
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor,registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+
+    def cantidadAsientos(self):
+        return len(self.asientos)
+    def verificarIntegridad(self):
+        valido = self.registro == self.motor.registro
+        if(valido):
+            for asiento in self.asientos:
+                if(asiento.registro!= self.registro):
+                    valido = False
+                    break
+        return "Auto original" if valido else "Las piezas no son originales"
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+    def asignarTipo(self, tipo):
+        tipo = tipo.lower()
+        if(tipo in ["electrico", "gasolina"]):
+            self.tipo = tipo
